39b605ffea0520a952ff203ad306b7c25450e445
Ritikabiswas <142112297+Ritikabiswas@users.noreply.github.com>
2025-08-31 14:57:04 +0000

    done

---
+++ memoryalloc.js
@@ -0,0 +1,17 @@
+//two types memory
+//stack(in primitive) heap( in non premitive)
+//primitive data types get only copy 
+/*let a=10
+let b=a
+b=20//now a=10,b=20*/
+//now
+/* let myobj={
+age:10,
+email:"amdnej@gmail.com"
+}
+let x=myobj
+x.age=30
+now both myobj.age and x.age both will be chnaged and it will be now 30 in both
+*/
+
+//----------------------------------Strings------------------------------------------------------------
