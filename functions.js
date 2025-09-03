dc4560d07150439526c1d9565a201063be1515e0
Ritikabiswas <142112297+Ritikabiswas@users.noreply.github.com>
2025-09-03 18:00:08 +0000

    updated

--- functions.js
+++ functions.js
@@ -18,3 +18,45 @@ function addone(num1,num2){
 const addtwo=function(num1,num2){
     return num1+num2
 }*/ 
+// this. refers to the current context
+const myobj={
+    name:"ritu",
+    age:21,
+    greeting:function(){
+        console.log(`${this.name} wellcome`)
+        //this inside the object will give the current object 
+    }
+}
+myobj.greeting()
+//console.log(this)//will give empty object {}//but in browser it will give window object
+/*we cannot use this.user name inside  function only console.log(this ) will give some objects
+const fun=function(){
+    console.log("normal function")
+    console.log(this)
+}
+fun()*/
+/*
+const arrowfun=()=>{
+   console.log("this is a arrow function")
+   console.log(this)//here it will give empty object 
+}
+arrowfun()*/
+// for one lined arrow function we can write it like this like 
+// const vname =(p1,p2)=> p1+p2
+// const uname=(p1,p2)=>(p1+p2)
+// console.log(vname(1,2))
+// console.log(uname(2,5)) 
+//for returning object we have to use parenthesis in arrow function
+// const wname=()=>({user:"tiyu"})
+// console.log(wname())
+
+// IIFE -immediately invoked function as we want sometime to load immediatedly and we donot want global variables to distrub 
+// our local variables so we make separate scope 
+function cha(){
+    console.log("used")
+}
+cha();
+//IIFE 
+(function cham(){
+  console.log("IIFE")
+})()
